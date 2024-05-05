b9507b9001bfe69a627dfd2ba1bd9dc9f046b276
jordi juny <jjunyc@uoc.edu>
2024-05-06 00:16:24 +0200

    chef finalitzat, index finalitzat, iniciat page3 amb about competition

--- src/assets/scripts/data.js
+++ src/assets/scripts/data.js
@@ -1,74 +1,56 @@
 const chefs = [
   {
-    "titol": "Chef One",
-    "lloc": "Los Angeles, LA",
-    "data": "",
-    "discount": "5",
-    "price": "6",
+    "titol": "Xavier Pellicer",
+    "lloc": "Barcelona, Catalunya",
     "detall": {
-        "text": "Aquest xef és un expert en la cuina mediterrània, amb una experiència de més de 15 anys en la preparació de plats inspirats en la regió. Ha treballat en diversos restaurants de renom a la costa mediterrània, guanyant reconeixement per les seves combinacions úniques de sabors i la seva presentació impecable.",
-        "multimedia": "avatar.png",
-        "sizemultimedia": []
+      "text": "Xavier Pellicer és un reconegut cuiner català amb una llarga trajectòria en el món de la cuina. Destaca per la seva passió per els productes locals i de proximitat, i pel seu compromís amb una cuina sostenible i respectuosa amb el medi ambient.",
+      "multimedia": "xp.jpeg",
+      "sizemultimedia": []
     }
   },
   {
-    "titol": "Chef Two",
-    "lloc": "Boston, BS",
-    "data": "",
-    "discount": "7",
-    "price": "68",
+    "titol": "Carme Ruscalleda",
+    "lloc": "Sant Pol de Mar, Catalunya",
     "detall": {
-        "text": "Aquest xef és un defensor de la cuina tradicional catalana, amb una passió per preservar i reinterpretar els plats clàssics. Amb més de dues dècades d'experiència, el seu restaurant a Barcelona és un referent de la cuina catalana moderna, oferint una experiència culinària única als comensals.",
-        "multimedia": "avatar.png",
-        "sizemultimedia": []
+      "text": "Carme Ruscalleda és una prestigiosa cuinera catalana amb estrella Michelin. Coneguda per la seva creativitat i per fusionar la tradició culinària catalana amb la innovació, ha portat la seva cuina a nivell internacional amb èxit.",
+      "multimedia": "cr5.webp",
+      "sizemultimedia": []
     }
   },
   {
-    "titol": "Chef Three",
-    "lloc": "Smallville, MI",
-    "data": "",
-    "discount": "5",
-    "price": "99",
+    "titol": "Albert Adrià",
+    "lloc": "Barcelona, Catalunya",
     "detall": {
-        "text": "Aquest xef és un expert en cuina internacional, amb una experiència diversificada en cuines de tot el món. És conegut per la seva capacitat de fusionar sabors i tècniques de diferents cultures per crear plats únics i innovadors que captiven els paladars dels comensals.",
-        "multimedia": "avatar.png",
-        "sizemultimedia": []
+      "text": "Albert Adrià és un reconegut pastisser i cuiner català que ha revolucionat el món de la gastronomia amb les seves creacions innovadores. Amb una trajectòria consolidada al costat del seu germà Ferran Adrià, ha guanyat prestigi i reconeixement internacional.",
+      "multimedia": "ad4.png",
+      "sizemultimedia": []
     }
   },
   {
-    "titol": "Chef Four",
-    "lloc": "New York, NY",
-    "data": "",
-    "discount": "3",
-    "price": "80",
+    "titol": "Nandu Jubany",
+    "lloc": "Vic, Catalunya",
     "detall": {
-        "text": "Aquest xef és un defensor de la cuina de mercat, amb una passió per utilitzar ingredients frescos i de temporada en els seus plats. Amb una formació en restaurants de tota Espanya, combina la tradició amb la innovació per crear experiències gastronòmiques úniques als seus clients.",
-        "multimedia": "avatar.png",
-        "sizemultimedia": []
+      "text": "Nandu Jubany és un reconegut xef català amb una estrella Michelin. La seva cuina destaca per la seva creativitat, la seva atenció als detalls i el seu amor per la tradició culinària catalana, que impregna tots els seus plats amb autenticitat i sabor.",
+      "multimedia": "nj2.png",
+      "sizemultimedia": []
     }
   },
   {
-    "titol": "Chef Five",
-    "lloc": "San Franciso, LA",
-    "data": "",
-    "discount": "13",
-    "price": "90",
+    "titol": "Ada Parellada",
+    "lloc": "Barcelona, Catalunya",
     "detall": {
-        "text": "Aquest xef té una dedicació especial a la cuina vegetariana, explorant la riquesa i diversitat dels productes vegetals. És conegut per la seva creativitat i el seu enfocament innovador a l'hora de preparar plats sans, saborosos i sostenibles que sorprenen i deleiten els comensals.",
-        "multimedia": "avatar.png",
-        "sizemultimedia": []
+      "text": "Ada Parellada és una cuinera i empresària catalana que ha revolucionat el panorama gastronòmic amb els seus restaurants i llibres de cuina. Apassionada defensora de la cuina casolana i de mercat, transmet autenticitat i calidesa en tots els seus plats.",
+      "multimedia": "avatar.png",
+      "sizemultimedia": []
     }
   },
   {
-    "titol": "Chef Six",
-    "lloc": "New York, NY",
-    "data": "",
-    "discount": "10",
-    "price": "110",
+    "titol": "Víctor Quintillà",
+    "lloc": "Girona, Catalunya",
     "detall": {
-        "text": "Aquest xef és un visionari de la cuina de fusió, combinant influències culinàries de tot el món per crear experiències gastronòmiques úniques. Amb una formació eclèctica i una passió per l'experimentació, ofereix una proposta culinària innovadora i emocionant als seus clients.",
-        "multimedia": "avatar.png",
-        "sizemultimedia": []
+      "text": "Víctor Quintillà és un jove i talentós cuiner català que ha destacat en el panorama gastronòmic amb la seva creativitat i passió per la cuina. Amb una trajectòria en restaurants de prestigi, porta una visió fresca i innovadora a tots els seus plats.",
+      "multimedia": "avatar.png",
+      "sizemultimedia": []
     }
   }
 ]
